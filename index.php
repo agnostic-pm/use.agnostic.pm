c26d39419577b8e6dc17bf4407180fccba56e887
Sainan <63328889+Sainan@users.noreply.github.com>
2024-08-03 08:47:15 +0200

    Use dynstat

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-header("Refresh: 0; url=https://github.com/agnostic-pm/use.agnostic.pm#readme");
-header("Content-Type: text/pluto-lang.org");
-?>
 package.preload["gitwit"] = function()
 	<?php require "gitwit.pluto"; ?>
 end
